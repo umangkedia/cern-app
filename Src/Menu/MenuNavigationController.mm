a6e882ffa9736ca304a69402b4ec387682f3ac4a
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-01-10 13:24:35 +0000

    Move navigation controller into the 'Menu' sub-dir.
    git-svn-id: http://root.cern.ch/svn/cern-app/trunk@222 59dca7bf-9055-4625-9011-bab61c547c97

---
+++ Src/Menu/MenuNavigationController.mm
@@ -0,0 +1,81 @@
+#import <cassert>
+
+#import "StaticInfoScrollViewController.h"
+#import "MenuNavigationController.h"
+#import "NewsTableViewController.h"
+#import "LiveTableViewController.h"
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
+
+@implementation MenuNavigationController
+
+//________________________________________________________________________________________
+- (id) initWithNibName : (NSString *) nibNameOrNil bundle : (NSBundle *) nibBundleOrNil
+{
+   return self = [super initWithNibName : nibNameOrNil bundle : nibBundleOrNil];
+}
+
+//________________________________________________________________________________________
+- (void) viewDidLoad
+{
+   [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+//________________________________________________________________________________________
+- (void) didReceiveMemoryWarning
+{
+   [super didReceiveMemoryWarning];
+   // Dispose of any resources that can be recreated.
+}
+
+//________________________________________________________________________________________
+- (void) viewWillAppear : (BOOL) animated
+{
+   [super viewWillAppear : animated];
+  
+   //We need a nice smooth shadow under our table.
+   self.view.layer.shadowOpacity = 0.75f;
+   self.view.layer.shadowRadius = 10.f;
+   self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+  
+   if (![self.slidingViewController.underLeftViewController isKindOfClass : [MenuViewController class]])
+      self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+
+   [self.view addGestureRecognizer : self.slidingViewController.panGesture];
+}
+
+//________________________________________________________________________________________
+- (void) addFeed : (NSString *) feed withName : (NSString *) feedName
+{
+   assert(feed != nil && "addFeed:, parameter 'feed' is nil");
+   assert(feedName != nil && "addFeed:, parameter 'feedName' is nil");
+   assert([self.topViewController isKindOfClass : [NewsTableViewController class]] &&
+          "addFeed:, topViewController is either nil, or has a wrong type - not a NewsTableViewController");
+
+   NewsTableViewController * const nt = (NewsTableViewController *)self.topViewController;
+   nt.navigationItem.title = feedName;
+   [nt.aggregator addFeedForURL : [NSURL URLWithString:feed]];
+}
+
+//________________________________________________________________________________________
+- (void) setExperiment : (CernAPP::LHCExperiment) experiment
+{
+   assert([self.topViewController isKindOfClass:[LiveTableViewController class]] &&
+          "setExperiment:, topViewController is either nil, or has a wrong type - not a LiveTableViewController");
+
+   LiveTableViewController * const lt = (LiveTableViewController *)self.topViewController;
+   lt.experiment = experiment;
+}
+
+//________________________________________________________________________________________
+- (void) setStaticInfo : (NSArray *) staticInfo withTitle : (NSString *) sectionName
+{
+   assert([self.topViewController isKindOfClass:[StaticInfoScrollViewController class]] &&
+          "setStaticInfo:withTitle:, topViewController is either nil, or has a wrong type - not a StaticInfoScrollViewController");
+   
+   StaticInfoScrollViewController * const st = (StaticInfoScrollViewController *)self.topViewController;
+   st.dataSource = staticInfo;
+}
+
+@end
