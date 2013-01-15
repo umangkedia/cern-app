3fc193c7d474b4b9db1df69689e6ff245b74705d
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-01-15 14:21:51 +0000

    Photos grid view controller and photos grid view cell are back.
    git-svn-id: http://root.cern.ch/svn/cern-app/trunk@263 59dca7bf-9055-4625-9011-bab61c547c97

---
+++ Src/CustomTables/PhotosGridViewController.h
@@ -0,0 +1,20 @@
+//
+//  PhotosViewController.h
+//  CERN App
+//
+//  Created by Eamon Ford on 6/27/12.
+//  Copyright (c) 2012 CERN. All rights reserved.
+//
+
+#import "AQGridViewController.h"
+#import "PhotoDownloader.h"
+#import "MWPhotoBrowser.h"
+#import "MBProgressHUD.h"
+
+@interface PhotosGridViewController : AQGridViewController<AQGridViewDataSource, AQGridViewDelegate, MWPhotoBrowserDelegate, PhotoDownloaderDelegate, MBProgressHUDDelegate>
+
+@property (nonatomic, strong) PhotoDownloader *photoDownloader;
+
+- (void) refresh;
+
+@end
