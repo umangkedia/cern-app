e9267df2f9f41b6934cc02dce78b7af3f29b50b0
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-01-16 17:14:21 +0000

    formatting.
    git-svn-id: http://root.cern.ch/svn/cern-app/trunk@273 59dca7bf-9055-4625-9011-bab61c547c97

--- Src/Core/AppDelegate.mm
+++ Src/Core/AppDelegate.mm
@@ -4,17 +4,16 @@
 
 @synthesize window = _window;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+//________________________________________________________________________________________
+- (BOOL) application : (UIApplication *) application didFinishLaunchingWithOptions : (NSDictionary *) launchOptions
 {
- //  [[UINavigationBar appearance] setTintColor : [UIColor colorWithPatternImage:[UIImage imageNamed :@"navbarback.png"]]];
-//   [[UINavigationBar appearance] setBackgroundImage : [UIImage imageNamed : @"navbarback.png"] forBarMetrics : UIBarMetricsDefault];
-
-   [[UIBarButtonItem appearance] setTintColor:[UIColor colorWithRed:0.f green:83.f/255.f blue:161.f/255.f alpha:1.f]];
+   [[UIBarButtonItem appearance] setTintColor : [UIColor colorWithRed : 0.f green : 83.f / 255.f blue : 161.f / 255.f alpha : 1.f]];
 
    return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+//________________________________________________________________________________________
+- (void) applicationWillResignActive : (UIApplication *) application
 {
   /*
    Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -22,7 +21,8 @@
    */
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+//________________________________________________________________________________________
+- (void) applicationDidEnterBackground : (UIApplication *) application
 {
   /*
    Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
@@ -30,21 +30,24 @@
    */
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
+//________________________________________________________________________________________
+- (void) applicationWillEnterForeground : (UIApplication *) application
 {
   /*
    Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
    */
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
+//________________________________________________________________________________________
+- (void) applicationDidBecomeActive : (UIApplication *) application
 {
   /*
    Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
    */
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
+//________________________________________________________________________________________
+- (void) applicationWillTerminate : (UIApplication *) application
 {
   /*
    Called when the application is about to terminate.
