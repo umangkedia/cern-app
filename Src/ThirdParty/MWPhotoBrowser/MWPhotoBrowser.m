b31d21131fbf56a9882f0e10e7114d2139046797
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-01-29 15:52:54 +0000

    Update css file for readability-produced content. Fix a warning in a MWPhotoBrowser.
    git-svn-id: http://root.cern.ch/svn/cern-app/trunk@327 59dca7bf-9055-4625-9011-bab61c547c97

--- Src/ThirdParty/MWPhotoBrowser/MWPhotoBrowser.m
+++ Src/ThirdParty/MWPhotoBrowser/MWPhotoBrowser.m
@@ -891,12 +891,8 @@ navigationBarBackgroundImageLandscapePhone = _navigationBarBackgroundImageLandsc
         }
         
         // Status Bar
-        if ([UIApplication instancesRespondToSelector:@selector(setStatusBarHidden:withAnimation:)]) {
-            [[UIApplication sharedApplication] setStatusBarHidden:hidden withAnimation:animated?UIStatusBarAnimationFade:UIStatusBarAnimationNone];
-        } else {
-            [[UIApplication sharedApplication] setStatusBarHidden:hidden animated:animated];
-        }
-        
+        [[UIApplication sharedApplication] setStatusBarHidden:hidden withAnimation : animated ? UIStatusBarAnimationFade : UIStatusBarAnimationNone];
+       
         // Get status bar height if visible
         if (![UIApplication sharedApplication].statusBarHidden) {
             CGRect statusBarFrame = [[UIApplication sharedApplication] statusBarFrame];
